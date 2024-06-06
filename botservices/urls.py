f5926b4ada96c884053ca2e839154f8fb55e3c3b
n48331 <unfolks@gmail.com>
2024-06-07 00:07:13 +0530

    files fix

--- botservices/urls.py
+++ botservices/urls.py
@@ -16,13 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from scbot_api import urls as scbot_urls
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('scbot_api/', include(scbot_urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
